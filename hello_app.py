0f8e67926d7b178fc4d48b7594249b715746fa7a
Денис Власов <denis.vlasov@phystech.edu>
2018-06-19 23:05:57 +0300

    initial commit

---
+++ hello_app.py
@@ -0,0 +1,90 @@
+from flask import Flask, render_template, request
+from flask_cors import CORS, cross_origin
+import base64
+import numpy as np
+from PIL import Image
+# import keras
+
+
+all_classes = np.load('ML/classes.npy')
+# model =  keras.models.load_model('ML/nnet_v1_recognized.h5')
+# model._make_predict_function()
+
+app = Flask(__name__)
+CORS(app, headers=['Content-Type'])
+# app.config["CACHE_TYPE"] = "null"
+
+
+@app.route('/', methods=["POST", "GET", "OPTIONS"])
+def main_page():
+    return render_template('index.html')
+
+
+@app.route('/hook', methods=["GET", "POST", 'OPTIONS'])
+def get_image():
+    if request.method == 'POST':
+        image_b64 = request.values['imageBase64']
+        image_encoded = image_b64.split(',')[1]
+        image = base64.decodebytes(image_encoded.encode('utf-8'))
+        with open('tmp/f1.jpg', 'wb') as f:
+            f.write(image)
+    return 'Picture saved'
+
+@app.route('/predict', methods=["GET", "POST", 'OPTIONS'])
+def predict_img():
+    if request.method != 'POST':
+        return 'Something is wrong'
+
+    image_b64 = request.values['imageBase64']
+    image_encoded = image_b64.split(',')[1]
+    image = base64.decodebytes(image_encoded.encode('utf-8'))
+    with open('tmp/img.jpg', 'wb') as f:
+        f.write(image)
+    image = Image.open('tmp/img.jpg')
+
+
+    results = []
+    for resample in [Image.HAMMING, 3]:
+        img = image.resize((28,28), resample=resample)
+        img = np.array(img)[:, :, 0].astype(float)
+        img = - img + img.max()
+        img /= img.max()/2
+        img = img.clip(0, 1)
+        preds = model.predict(img.reshape(1, 28, 28, 1))
+
+        if np.isnan(img).any():
+            results.append('Drew something first.')
+        elif preds.max() < 0.6:
+            results.append('I have no idea what you drew.')
+        else:
+            results.append("I'm {}% sure it's a {}.".format(
+                round(100*preds.max(), 1),  all_classes[preds.argmax()]))
+
+    return '<br>'.join(results)
+
+
+@app.route('/get_ideas', methods=["GET", "POST", 'OPTIONS'])
+def get_some_ideas():
+    #result = '<br>'.join(np.random.choice(all_classes, 5, False)) + '<br>'
+
+    lines = []
+    line = ''
+    for w in sorted(np.random.choice(all_classes, 10, True)):
+        if len(line) + len(w) >= 38:
+            lines.append(line)
+            line = ''
+        line += w + ', '
+    lines.append(line[:-2])
+
+    return '<br>'.join(lines) + '.<br>'
+
+
+@app.route('/hello')
+def hello_world():
+    return 'Hello, World!'
+
+
+if __name__ == "__main__":
+    app.config["CACHE_TYPE"] = "null"
+
+    app.run()
---
+++ static/draw.js
@@ -0,0 +1,132 @@
+//for canvas drawing used code from here: https://github.com/zealerww/digits_recognition/blob/master/digits_recognition/static/draw.js
+var drawing = false;
+var context;
+var offset_left = 0;
+var offset_top = 0;
+
+function start_canvas () {
+    var canvas = document.getElementById ("Canvas");
+    context = canvas.getContext ("2d");
+    canvas.onmousedown = function (event) {mousedown(event)};
+    canvas.onmousemove = function (event) {mousemove(event)};
+    canvas.onmouseup   = function (event) {mouseup(event)};
+    for (var o = canvas; o ; o = o.offsetParent) {
+    offset_left += (o.offsetLeft - o.scrollLeft);
+    offset_top  += (o.offsetTop - o.scrollTop);
+    }
+    draw();
+}
+
+function getPosition(evt) {
+    evt = (evt) ?  evt : ((event) ? event : null);
+    var left = 0;
+    var top = 0;
+    var canvas = document.getElementById("Canvas");
+
+    if (evt.pageX) {
+    left = evt.pageX;
+    top  = evt.pageY;
+    } else if (document.documentElement.scrollLeft) {
+    left = evt.clientX + document.documentElement.scrollLeft;
+    top  = evt.clientY + document.documentElement.scrollTop;
+    } else  {
+    left = evt.clientX + document.body.scrollLeft;
+    top  = evt.clientY + document.body.scrollTop;
+    }
+    left -= offset_left;
+    top -= offset_top;
+
+    return {x : left, y : top}; 
+}
+
+function
+mousedown(event) {
+    drawing = true;
+    var location = getPosition(event);
+    context.lineWidth = 3.0;
+    context.strokeStyle="#000000";
+    context.beginPath();
+    context.moveTo(location.x,location.y);
+}
+
+function
+mousemove(event) {
+    if (!drawing) 
+        return;
+    var location = getPosition(event);
+    context.lineTo(location.x,location.y);
+    context.stroke();
+}
+
+
+
+function
+mouseup(event) {
+    if (!drawing) 
+        return;
+    mousemove(event);
+	context.closePath();
+    drawing = false;
+}
+
+function draw() {
+    context.fillStyle = '#ffffff';
+    context.fillRect(0, 0, 256, 256);
+}
+
+
+function clearCanvas() {
+    context.clearRect (0, 0, 256, 256);
+    draw();
+}
+
+function saveImg()
+{
+	document.getElementById("prediction_result").innerHTML = "Making prediction...";
+	var canvas = document.getElementById("Canvas");
+	var dataURL = canvas.toDataURL('image/jpg');
+	$.ajax({
+	  type: "POST",
+	  url: "/hook",
+	  data:{
+		imageBase64: dataURL
+		}
+	}).done(function(response) {
+	  console.log(response)
+	  document.getElementById("prediction_result").innerHTML = response
+	});
+}
+
+function predictImg()
+{
+    document.getElementById("prediction_result").innerHTML = "Making prediction...";
+    var canvas = document.getElementById("Canvas");
+    var dataURL = canvas.toDataURL('image/jpg');
+    $.ajax({
+      type: "POST",
+      url: "/predict",
+      data:{
+        imageBase64: dataURL
+        }
+    }).done(function(response) {
+      console.log(response)
+      document.getElementById("prediction_result").innerHTML = response
+    });
+}
+
+function getIdeas()
+{
+    $.ajax({
+      type: "POST",
+      url: "/get_ideas",
+      data:{}
+    }).done(function(response) {
+      console.log(response)
+      document.getElementById("get_ideas_header").innerHTML = '<br>Try one of those:<br>' 
+      document.getElementById("get_ideas_result").innerHTML = response
+    });
+    
+}
+
+
+onload = start_canvas;
