e55bdc605752d5859df45309fafe244efac241b9
Денис Власов <denis.vlasov@phystech.edu>
2018-07-24 09:20:14 +0300

    removed torch

--- model/predictor.py
+++ model/predictor.py
@@ -2,8 +2,6 @@ import numpy as np
 from PIL import Image, ImageOps
 import keras
 import base64
-from ML.torch_models import InceptionBasedNet
-import torch
 
 
 class Predictor(object):
@@ -14,14 +12,6 @@ class Predictor(object):
         self.mean_global = np.load(path+'mean_global.npy')[0]
         self.model = keras.models.load_model(path+'nnet_96_aug_v1.h5')
         self.model._make_predict_function()
-
-        self.torch_model = InceptionBasedNet(
-            self.all_classes.shape[0], (1, self.px, self.px))
-        self.torch_model.load_state_dict(
-            torch.load(
-                path+'InceptionBasedNet.t7',
-                map_location='cpu'))
-        self.torch_model.eval()
         self.image = None
 
     def decode_image(self, request):
@@ -70,27 +60,16 @@ class Predictor(object):
         if self.image is None:
             return 'Draw something first.'
 
-        result = []
-        for mode in ['Keras', 'Torch']:
-            if mode == 'Keras':
-                nnet_name = 'Keras, convolution nnet'
-                img = self.image.reshape(1, self.px, self.px, 1)
-                preds = self.model.predict(img)[0]
-            elif mode == 'Torch':
-                nnet_name = 'Torch, Inception v3 nnet'
-                img = self.image.reshape(1, 1, self.px, self.px)
-                img = torch.Tensor(img)
-                preds = self.torch_model(img).detach().numpy()
-
-            res = nnet_name + ':<br>'
-            if preds.max() < 0.1:
-                res += "I have no idea what you drew."
-            else:
-                res += "I'm {}% sure it's a {}.".format(
-                    round(100*preds.max(), 1), self.all_classes[preds.argmax()]
-                    )
-            result.append(res+'<br>')
-        return "<br>".join(result)
+        img = self.image.reshape(1, self.px, self.px, 1)
+        preds = self.model.predict(img)[0]
+        if preds.max() < 0.1:
+            res = "I have no idea what you drew."
+        else:
+            res = "I'm {}% sure it's a {}.".format(
+                round(100*preds.max(), 1), self.all_classes[preds.argmax()]
+                )
+        res += '<br>'
+        return res
 
     def get_classes_example(self, num_classes):
         return sorted(np.random.choice(self.all_classes, num_classes, False))
